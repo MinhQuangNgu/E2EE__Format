0c6d5823c4b3e56b2b47ee9c55e3d27dfe1890c6
QuangNM <quangnmhe160393@fpt.edu.vn>
2023-04-03 16:56:31 +0700

    updating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import { useEffect, useState } from "react";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [publicKey, setPublicKey] = useState("");
+	useEffect(() => {
+		if (!publicKey) {
+			axios.get("/end_end").then(async (res) => {
+				const data = await importPublicKey(res?.data?.publicKey);
+				setPublicKey(data);
+			});
+		}
+	}, []);
+
+	useEffect(() => {}, []);
+	function importPublicKey(pem) {
+		const pemHeader = "-----BEGIN PRIVATE KEY-----";
+		const pemFooter = "-----END PRIVATE KEY-----";
+		const pemContents = pem.substring(
+			pemHeader.length,
+			pem.length - pemFooter.length
+		);
+		const binaryDerString = window.atob(pemContents);
+		const binaryDer = str2ab(binaryDerString);
+		return window.crypto.subtle.importKey(
+			"spki",
+			binaryDer,
+			{
+				name: "RSA-OAEP",
+				hash: { name: "SHA-256" },
+			},
+			true,
+			["encrypt"]
+		);
+	}
+	function str2ab(str) {
+		const buf = new ArrayBuffer(str.length);
+		const bufView = new Uint8Array(buf);
+		for (let i = 0, strLen = str.length; i < strLen; i++) {
+			bufView[i] = str.charCodeAt(i);
+		}
+		return buf;
+	}
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
